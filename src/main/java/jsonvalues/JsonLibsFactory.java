56141cfcc439e2b79ef08424285643331a048d05
imrafaelmerino <imrafael.merino@gmail.com>
2020-03-14 23:10:48 +0100

    feat: 🎸 close #112
    serializers are loaded programmatically instead of via ServiceLoader

--- src/main/java/jsonvalues/JsonLibsFactory.java
+++ src/main/java/jsonvalues/JsonLibsFactory.java
@@ -1,6 +1,10 @@
 package jsonvalues;
 
 import com.dslplatform.json.DslJson;
+import com.dslplatform.json.JsonWriter;
+import com.dslplatform.json.serializers.JsArraySerializer;
+import com.dslplatform.json.serializers.JsObjSerializer;
+import com.dslplatform.json.serializers.JsValueSerializer;
 import com.fasterxml.jackson.core.JsonFactory;
 
 import java.io.ByteArrayOutputStream;
@@ -14,6 +18,22 @@ class JsonLibsFactory
 {
     static JsonFactory jackson = new JsonFactory();
     static DslJson<Object> dslJson = new DslJson<>();
+
+
+  static {
+    final JsValueSerializer valueSerializer = new JsValueSerializer();
+    final JsonWriter.WriteObject<JsObj> objSerializer = new JsObjSerializer<>(valueSerializer);
+    final JsonWriter.WriteObject<JsArray> arraySerializer = new JsArraySerializer<>(valueSerializer);
+     valueSerializer.setArraySerializer(arraySerializer);
+     valueSerializer.setObjectSerializer(objSerializer);
+     dslJson.registerWriter(JsObj.class,
+                            objSerializer
+                            );
+
+     dslJson.registerWriter(JsArray.class,
+                            arraySerializer
+                           );
+  }
 
     /** Returns the string representation of this Json
      *
