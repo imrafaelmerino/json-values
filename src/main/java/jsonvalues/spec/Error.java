5de879356eafd5c3c168c9709b737d03f4cb10bb
imrafaelmerino <imrafael.merino@gmail.com>
2020-08-18 21:20:50 +0200

    fix: 🐛 hashcode and equals methods in JsErrorPair and Error
    Closes: #153

--- src/main/java/jsonvalues/spec/Error.java
+++ src/main/java/jsonvalues/spec/Error.java
@@ -2,6 +2,8 @@ package jsonvalues.spec;
 
 import jsonvalues.JsValue;
 
+import java.util.Objects;
+
 public class Error {
     public final ERROR_CODE code;
     public final JsValue value;
@@ -19,5 +21,21 @@ public class Error {
                 "code=" + code +
                 ", value=" + value +
                 ')';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Error error = (Error) o;
+        return code == error.code &&
+                value.equals(error.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code,
+                            value
+                           );
     }
 }
--- src/main/java/jsonvalues/spec/JsErrorPair.java
+++ src/main/java/jsonvalues/spec/JsErrorPair.java
@@ -2,6 +2,8 @@ package jsonvalues.spec;
 
 import jsonvalues.JsPath;
 
+import java.util.Objects;
+
 import static java.util.Objects.requireNonNull;
 
 public class JsErrorPair {
@@ -29,5 +31,21 @@ public class JsErrorPair {
                 "path=" + path +
                 ", error=" + error +
                 ')';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final JsErrorPair that = (JsErrorPair) o;
+        return path.equals(that.path) &&
+                error.equals(that.error);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path,
+                            error
+                           );
     }
 }
