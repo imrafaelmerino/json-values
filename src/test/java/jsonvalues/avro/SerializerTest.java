3a25cdea14e5048fd342c9fc62cdaf8d53774141
Rafael Merino García <imrafaelmerino@gmail.com>
2023-10-31 20:18:46 +0100

    from genericdata to json-values

--- src/test/java/jsonvalues/avro/SerializerTest.java
+++ src/test/java/jsonvalues/avro/SerializerTest.java
@@ -5,12 +5,181 @@ import jsonvalues.spec.AvroAttBuilder;
 import jsonvalues.spec.JsObjSpec;
 import jsonvalues.spec.JsSpecs;
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericArray;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
 public class SerializerTest {
+
+    public static JsArray toJsArray(GenericArray<?> genericArray) {
+        JsArray jsArray = JsArray.empty();
+        Schema elementType = genericArray.getSchema().getElementType();
+        for (Object item : genericArray) {
+            jsArray = jsArray.append(toJsValue(item, elementType));
+        }
+        return jsArray;
+    }
+
+    public static JsObj fromMapToJsOb(Object value, Schema valueSchema) {
+        JsObj jsObj = JsObj.empty();
+        Schema valueTypeSchema = valueSchema.getValueType();
+        for (Map.Entry<?, ?> entry : ((Map<?, ?>) value).entrySet()) {
+            String key = entry.getKey().toString();
+            Object entryValue = entry.getValue();
+            JsValue jsValue = toJsValue(entryValue, valueTypeSchema);
+            jsObj = jsObj.set(key, jsValue);
+        }
+        return jsObj;
+    }
+
+    public static JsValue fromUnionToJsValue(Object value, List<Schema> unionSchemas) {
+        for (Schema schema : unionSchemas) {
+            try {
+                return toJsValue(value, schema);
+            } catch (Exception e) {
+                // If conversion fails, try the next schema in the union
+            }
+        }
+        throw new IllegalArgumentException("Value does not match any of the union types");
+    }
+
+
+    public static JsValue toJsValue(Object value, Schema schema) {
+        var type = schema.getType();
+
+        if (value == null) {
+            if (type == Schema.Type.NULL) {
+                return JsNull.NULL;
+            } else {
+                throw new IllegalArgumentException("Value is null, but the schema type is not NULL");
+            }
+        }
+
+        if (type == Schema.Type.BOOLEAN) {
+            if (value instanceof Boolean) {
+                return JsBool.of((Boolean) value);
+            } else {
+                throw new IllegalArgumentException("Expected a Boolean value for BOOLEAN schema type");
+            }
+        }
+
+        if (type == Schema.Type.STRING) {
+
+            if (value instanceof CharSequence) {
+                if (schema.getLogicalType().equals("bigdecimal"))
+                    return JsBigDec.of(new BigDecimal(value.toString()));
+                if (schema.getLogicalType().equals("biginteger"))
+                    return JsBigInt.of(new BigInteger(value.toString()));
+                if (schema.getLogicalType().equals("iso-8601"))
+                    return JsInstant.of(value.toString());
+                return JsStr.of(value.toString());
+            } else {
+                throw new IllegalArgumentException("Expected a CharSequence value for STRING schema type");
+            }
+        }
+        if (type == Schema.Type.ENUM) {
+            if (value instanceof CharSequence) {
+                return JsStr.of(value.toString());
+            } else {
+                throw new IllegalArgumentException("Expected a CharSequence value for ENUM schema type");
+            }
+        }
+
+        if (type == Schema.Type.INT) {
+            String logicalType = schema.getProp("logicalType");
+            if (logicalType != null) {
+                if (logicalType.equals("timestamp-millis")) {
+                    Instant instant = Instant.ofEpochMilli((long) value);
+                    return JsInstant.of(instant);
+                }
+                else if (logicalType.equals("timestamp-micros")) {
+                    // Handle timestamp-micros logical type (convert int to timestamp)
+                    Instant instant = Instant.ofEpochMilli(((long) value) / 1000);
+                    return JsInstant.of(instant);
+                }
+                // Handle other logical types as needed
+            }
+            // If no logical type, treat the int as a JsInt
+            return JsInt.of((int) value);
+        }
+
+        if (type == Schema.Type.LONG) {
+            if (value instanceof Long) {
+                return JsLong.of((long) value);
+            } else {
+                throw new IllegalArgumentException("Expected a Long value for LONG schema type");
+            }
+        }
+
+        if (type == Schema.Type.DOUBLE || type == Schema.Type.FLOAT) {
+            if (value instanceof Number) {
+                return JsDouble.of(((Number) value).doubleValue());
+            } else {
+                throw new IllegalArgumentException("Expected a Number value for DOUBLE or FLOAT schema type");
+            }
+        }
+
+        if (type == Schema.Type.BYTES || type == Schema.Type.FIXED) {
+            if (value instanceof byte[]) {
+                return JsBinary.of((byte[]) value);
+            } else {
+                throw new IllegalArgumentException("Expected a byte array value for BYTES or FIXED schema type");
+            }
+        }
+
+        if (type == Schema.Type.RECORD) {
+            if (value instanceof GenericRecord) {
+                return toJsObj((GenericRecord) value);
+            } else {
+                throw new IllegalArgumentException("Expected a GenericRecord value for RECORD schema type");
+            }
+        }
+
+        if (type == Schema.Type.ARRAY) {
+            if (value instanceof GenericArray<?>) {
+                return toJsArray((GenericArray<?>) value);
+            } else {
+                throw new IllegalArgumentException("Expected a GenericArray value for ARRAY schema type");
+            }
+        }
+
+        if (type == Schema.Type.MAP) {
+            if (value instanceof Map) {
+                return fromMapToJsOb(value, schema.getValueType());
+            } else {
+                throw new IllegalArgumentException("Expected a Map value for MAP schema type");
+            }
+        }
+
+        if (type == Schema.Type.UNION) {
+            return fromUnionToJsValue(value, schema.getTypes());
+        }
+
+        throw new IllegalArgumentException("Type %s not supported".formatted(type.getName()));
+    }
+
+
+    public static JsObj toJsObj(GenericRecord record) {
+        Schema schema = record.getSchema();
+        JsObj result = JsObj.empty();
+        assert schema.getType() == Schema.Type.RECORD;
+        for (Schema.Field field : schema.getFields()) {
+            result = result.set(field.name(), toJsValue(record.get(field.name()),
+                                                        field.schema()));
+        }
+
+        return result;
+
+    }
 
     public static GenericRecordBuilder toAvro(String key,
                                               JsValue value,
@@ -60,7 +229,7 @@ public class SerializerTest {
         assert (schema.getType() == Schema.Type.ARRAY);
 
         if (value instanceof JsStr js) {
-            array.set(index, null);
+            array.set(index, js.value);
         } else if (value instanceof JsInt js) {
             array.set(index, js.value);
         } else if (value instanceof JsLong js) {
@@ -133,11 +302,18 @@ public class SerializerTest {
         System.out.println(string);
         Schema avroSchema = parser.parse(string);
 
-        GenericRecord record = toAvro(JsObj.of("a", JsStr.of("hi")),
+        JsObj obj1 = JsObj.of("a", JsStr.of("hi"),
+                              "b", JsInt.of(1));
+        GenericRecord record = toAvro(obj1,
                                       avroSchema);
 
         System.out.println(record);
 
+        JsObj obj2 = toJsObj(record);
+
+        System.out.println(obj2);
+
+        Assertions.assertEquals(obj1, obj2);
 
     }
 }
